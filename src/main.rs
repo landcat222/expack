091245aba54ae4235772b60e8bd455e35c6a5c1d
landcat222 <67131672+landcat222@users.noreply.github.com>
2021-03-06 11:03:19 +0900

    Delete EXPATH variable

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use once_cell::sync::OnceCell;
 use std::path::PathBuf;
 mod init;
 
-pub(crate) static EXPATH: OnceCell<PathBuf> = OnceCell::new();
-
 fn main() {
     init::init();
     println!("Hello, world!");
