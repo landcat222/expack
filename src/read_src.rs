d96dbb9f29fd175aa694b1e71048bebd79f158e8
landcat222 <67131672+landcat222@users.noreply.github.com>
2021-03-07 18:38:48 +0900

    Add format function

--- src/read_src.rs
+++ src/read_src.rs
@@ -4,3 +4,12 @@ use std::fs::read_to_string;
 fn read_file(src: PathBuf) -> String {
     read_to_string(src.display().to_string()).expect("File not found")
 }
+
+fn format(src: String) -> String {
+    let pieces = src.split("\n");
+    let mut result = String::new();
+    for i in pieces {
+        result = format!("{}\n{}",result,i.trim());
+    }
+    result
+}
